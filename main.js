bb94be59132e68f700b82e21f69208516d241d58
xeratun <nutarex@gmail.com>
2017-10-10 16:25:45 -0400

    Added Menu Items
    Menu:
    Menu now allows for switching between the stable and beta websites to
    let user choose which to use. Stable loads by default.
    Want to look into saving state across loads to remember which version
    was last open.
    
    Media:
    Media keys were only working for beta. They now check which version is
    loaded and works appropriately in either.
    Realized media keys only work when the window is in focus, will have to
    look into that.

--- main.js
+++ main.js
@@ -3,40 +3,79 @@ const electron = require( 'electron' )
 // Module to control application life.
 const app = electron.app
 
+// Module to control menu options
+const Menu = electron.Menu;
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
-const path = require( 'path' )
-const url  = 'https://playbeta.pocketcasts.com'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+function loadPocketCasts(useBeta){
+	if(useBeta){
+		mainWindow.loadURL('file://' + __dirname + '/beta.html')
+	} else {
+		mainWindow.loadURL('file://' + __dirname + '/index.html')
+	}
+}
 
 function createWindow() {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
 		width: 1080,
 		height: 720
-	})
+	});
+	
+	// Populate menu items for the window
+	const menuTemplate = [
+		{
+			label: 'File',
+			submenu: [
+			{
+				label: 'Open DevTools',
+				click: () => {
+					mainWindow.webContents.openDevTools();
+				}
+			}, {
+				label: 'Quit',
+				click: () => {
+					app.quit();
+				}
+			}
+			]
+		},
+		{
+			label: 'PocketCasts',
+			submenu: [
+			{
+				label: 'Stable',
+				click: () => {
+					loadPocketCasts(false);
+				}
+			}, {
+				label: 'Beta',
+				click: () => {
+					loadPocketCasts(true);
+				}
+			}
+			]
+		}
+	];
+
+	const menu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(menu);
 	
 	var useBeta = false;
-	
-	if(useBeta){
-		mainWindow.loadURL('file://' + __dirname + '/beta.html')
-	} 	else {
-		mainWindow.loadURL('file://' + __dirname + '/index.html')
-	}
-	
-	// Open the DevTools.
-	// mainWindow.webContents.openDevTools()
+	loadPocketCasts(useBeta);
 	
 	// Emitted when the window is closed.
 	mainWindow.on( 'closed', function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
+		mainWindow = null;
 	})
 }
 
@@ -50,7 +89,7 @@ app.on( 'window-all-closed', function () {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if ( process.platform !== 'darwin' ) {
-		app.quit()
+		app.quit();
 	}
 })
 
@@ -58,7 +97,7 @@ app.on( 'activate', function () {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if ( mainWindow === null ) {
-		createWindow()
+		createWindow();
 	}
 })
 
--- media.js
+++ media.js
@@ -3,14 +3,26 @@ const {ipcRenderer} = require('electron');
 
 // Execute media key commands
 ipcRenderer.on('media_play_pause', function(){
-    document.getElementsByClassName('animated-play-button')[0].click();
-	document.getElementsByClassName('play_pause_button')[0].click();
+    var btn = document.getElementsByClassName('play_pause_button')
+    if(btn.length > 0){
+        document.getElementsByClassName('play_pause_button')[0].click();
+    } else {
+        document.getElementsByClassName('animated-play-button')[0].click();
+    }
 });
 ipcRenderer.on('media_next',function(event,data){
-    document.getElementsByClassName('skip-forward-button')[0].click();
-    document.getElementsByClassName('skip_forward_button')[0].click();
+    var btn = document.getElementsByClassName('skip_forward_button')
+    if(btn.length > 0){
+        document.getElementsByClassName('skip_forward_button')[0].click();
+    } else {
+        document.getElementsByClassName('skip-forward-button')[0].click();
+    }
 });
 ipcRenderer.on('media_prev',function(event,data){
-    document.getElementsByClassName('skip-back-button')[0].click();
-    document.getElementsByClassName('skip_back_button')[0].click();
+    var btn = document.getElementsByClassName('skip_back_button')
+    if(btn.length > 0){
+        document.getElementsByClassName('skip_back_button')[0].click();
+    } else {
+        document.getElementsByClassName('skip-back-button')[0].click();
+    }
 });
