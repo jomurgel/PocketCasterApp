6e6b6f31c1298803c0196b09fc6c35d19b6b55f5
Vincent Trivett <trivett@gmail.com>
2018-10-20 11:32:32 -0400

    prettier

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const electron = require( 'electron' )
-const path = require( 'path' )
+const electron = require('electron')
+const path = require('path')
 const nativeImage = electron.nativeImage
 
-let appIcon = nativeImage.createFromPath( path.join( __dirname, 'assets/icons/png/256x256.png' ) )
+let appIcon = nativeImage.createFromPath(path.join(__dirname, 'assets/icons/png/256x256.png'))
 
 // Module to control application life.
 const app = electron.app
@@ -20,8 +20,8 @@ const globalShortcut = electron.globalShortcut;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function loadPocketCasts(useBeta){
-	if(useBeta){
+function loadPocketCasts(useBeta) {
+	if (useBeta) {
 		mainWindow.loadURL('file://' + __dirname + '/beta.html')
 	} else {
 		mainWindow.loadURL('file://' + __dirname + '/index.html')
@@ -40,7 +40,7 @@ function createWindow() {
 	let registerKeys = function () {
 
 		const reg = globalShortcut.register('MediaPlayPause', () => {
-			 // console.log('mediaplaypause pressed');
+			// console.log('mediaplaypause pressed');
 			mainWindow.webContents.sendInputEvent({
 				type: "keyDown",
 				keyCode: "\u0020"
@@ -75,53 +75,53 @@ function createWindow() {
 			});
 		});
 	};
-	
+
 	// Populate menu items for the window
 	const menuTemplate = [
 		{
 			label: 'File',
 			submenu: [
-			{
-				label: 'Open DevTools',
-				click: () => {
-					mainWindow.webContents.openDevTools();
-				},
-				accelerator: 'CmdOrCtrl+Alt+I'
-			}, {
-				label: 'Quit',
-				click: () => {
-					app.quit();
-				},
-				accelerator: 'CmdOrCtrl+Q'
-			}
+				{
+					label: 'Open DevTools',
+					click: () => {
+						mainWindow.webContents.openDevTools();
+					},
+					accelerator: 'CmdOrCtrl+Alt+I'
+				}, {
+					label: 'Quit',
+					click: () => {
+						app.quit();
+					},
+					accelerator: 'CmdOrCtrl+Q'
+				}
 			]
 		},
 		{
 			label: 'Version',
 			submenu: [
-			{
-				label: 'Stable',
-				click: () => {
-					loadPocketCasts(false);
+				{
+					label: 'Stable',
+					click: () => {
+						loadPocketCasts(false);
+					}
+				}, {
+					label: 'Beta',
+					click: () => {
+						loadPocketCasts(true);
+					}
 				}
-			}, {
-				label: 'Beta',
-				click: () => {
-					loadPocketCasts(true);
-				}
-			}
 			]
 		}
 	];
 
 	const menu = Menu.buildFromTemplate(menuTemplate);
 	Menu.setApplicationMenu(menu);
-	
+
 	var useBeta = false;
 	loadPocketCasts(useBeta);
-	
+
 	// Emitted when the window is closed.
-	mainWindow.on( 'closed', function () {
+	mainWindow.on('closed', function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -136,21 +136,21 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on( 'ready', createWindow )
+app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on( 'window-all-closed', function () {
+app.on('window-all-closed', function () {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
-	if ( process.platform !== 'darwin' ) {
+	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 })
 
-app.on( 'activate', function () {
+app.on('activate', function () {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if ( mainWindow === null ) {
+	if (mainWindow === null) {
 		createWindow();
 	}
 })
